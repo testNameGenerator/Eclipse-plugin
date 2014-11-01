725c59419c3dfb3df54ec5abe91660f437e914e4
sorinstanila <sorinvaler.stanila@gmail.com>
2014-11-01 16:18:12 +0200

    little refactoring of the code for Convertor.java stage 1 for #3 test ammend commit

--- src/testnamegenerator/handlers/Convertor.java
+++ src/testnamegenerator/handlers/Convertor.java
@@ -4,6 +4,7 @@ import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.runtime.Platform;
+import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.IRegion;
 import org.eclipse.jface.text.ITextSelection;
@@ -17,135 +18,228 @@ import org.eclipse.ui.texteditor.ITextEditor;
 import com.sun.xml.ws.util.StringUtils;
 
 public class Convertor extends AbstractHandler {
-	
-	public Object execute(ExecutionEvent event) throws ExecutionException {
+    private EclipseHelperContext eclipseApi;
+
+    public Object execute(ExecutionEvent event) throws ExecutionException {
+	try {
+	    this.eclipseApi = new EclipseHelperContext();
+	    IDocument doc = eclipseApi.getDocument();
+	    int lineNumber = eclipseApi.getCurrentLineNumber();
+
+	    if (lineNumber >= 0) {
+		String originalText = eclipseApi.getLineContents(lineNumber);
+		String preparedText = TextHelper.getPreparedTestMethod(
+			originalText, eclipseApi.getTabChar());
+		eclipseApi.generateTestMethod(preparedText, lineNumber, doc);
+	    }
+
+	} catch (Exception e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	}
+
+	return null;
+    }
+
+    public static class TextHelper {
+	private static final String regex = "\\P{Alnum}";
+
+	/**
+	 * @return the regex
+	 */
+	public static String getRegex() {
+	    return regex;
+	}
+
+	/**
+	 * 
+	 * @param init
+	 * @return
+	 */
+	private static String toCamelCase(final String init) {
+	    final StringBuilder ret = new StringBuilder(init.length());
+	    for (final String word : init.split(" ")) {
+		ret.append(StringUtils.capitalize(word));
+	    }
+	    return ret.toString();
+	}
+
+	/**
+	 * 
+	 * @param originalText
+	 * @param tab
+	 * @return
+	 */
+	public static String getPreparedTestMethod(String originalText,
+		String tab) {
+	    String commentText = "\n" + tab + "/**\n" + tab + " * "
+		    + originalText.replace("*/", "* /") + "\n" + tab + " */\n"
+		    + tab + "";
+	    String methodNameText = originalText.replaceAll(getRegex(), " ")
+		    .trim();
+	    methodNameText = (methodNameText.isEmpty()) ? "Blank"
+		    : toCamelCase(methodNameText);
+	    String methodText = "public function test"
+		    + methodNameText.replace(" ", "") + "() {\n" + tab + ""
+		    + tab + "$this->markTestIncomplete('implement me...');\n"
+		    + tab + "}\n";
+	    return commentText + methodText;
+	}
+    }
+
+    public class EclipseHelperContext {
+
+	/**
+	 * 
+	 * @param lineNumber
+	 * @return
+	 */
+	public String getLineContents(int lineNumber) {
+	    IDocument doc = this.getDocument();
+	    ITextEditor editor = this.getEditor();
+	    String selection = null;
+	    if (editor != null) {
 		try {
-			IDocument doc     = this.getDocument();
-			ITextSelection ts = this.getSelection();
-			int lineNumber    = this.getCurrentLineNumber(ts);
-			
-			if (lineNumber >= 0) {
-				String originalText = this.getLineContents(lineNumber, doc);
-				String preparedText = this.getPreparedTestMethod(originalText);
-				this.generateTestMethod(preparedText, lineNumber, doc);
-			}
+		    selectAndRevealText(lineNumber, doc, editor);
+		    ISelection sel = editor.getSelectionProvider()
+			    .getSelection();
 
-		} catch(Exception e) {}
-
-		return null;
-	}
-	
-	private void generateTestMethod(String preparedText, int lineNumber, IDocument doc)
-	{
-		ITextEditor editor = this.getEditor();
-		if (editor == null
-				|| !editor.isEditable()
-				|| !this.getPage().getActivePartReference().getId().toString()
-						.toLowerCase().contains(".php.editor")) {
-			return;
+		    if (sel instanceof TextSelection) {
+			final TextSelection textSel = (TextSelection) sel;
+			selection = (textSel.getText().toString()).trim();
+		    }
+		} catch (Exception e) {
+		    // TODO Auto-generated catch block
+		    e.printStackTrace();
 		}
-		try {
-			IRegion lineInfo = doc.getLineInformation(lineNumber);
-			editor.selectAndReveal(lineInfo.getOffset(), lineInfo.getLength());
-			ISelection sel   = editor.getSelectionProvider().getSelection();
-	
-			if (sel instanceof TextSelection) {
-				final TextSelection textSel = (TextSelection)sel;
-				doc.replace(textSel.getOffset(), textSel.getLength(), preparedText);
-	
-				// select temporary text
-				lineInfo = doc.getLineInformation(lineNumber + 5);
+	    }
+	    return selection;
+	}
 
-				Boolean useSpaces = this.useSpacesForTabs();
-				int offset = (useSpaces) ? 2 * this.getTabChar().length() : 2;
-				editor.selectAndReveal(lineInfo.getOffset() + offset, lineInfo.getLength() - offset); // remove tabs from selection
-			}
-		} catch(Exception e) {}
+	/**
+	 * 
+	 * @return activePage or null
+	 */
+	private IWorkbenchPage getPage() {
+	    return PlatformUI.getWorkbench().getActiveWorkbenchWindow()
+		    .getActivePage();
 	}
-	
-	private String getPreparedTestMethod(String originalText)
-	{
-		String tab = this.getTabChar();
-		String commentText    = "\n" + tab + "/**\n" + tab + " * " + originalText.replace("*/", "* /") + "\n" + tab + " */\n" + tab + "";
-		String methodNameText = originalText.replaceAll("\\P{Alnum}", " ").trim();
-		methodNameText        = (methodNameText.isEmpty()) ? "Blank" : this.toCamelCase(methodNameText);
-		String methodText     = "public function test" + methodNameText.replace(" ", "") + "() {\n" + tab + "" + tab + "$this->markTestIncomplete('implement me...');\n" + tab + "}\n";
-		return commentText + methodText;
-	}
-	
-	private String getLineContents(int lineNumber, IDocument doc)
-	{
-		ITextEditor editor = this.getEditor();
-		if (editor != null) {
-			try {
-				IRegion lineInfo = doc.getLineInformation(lineNumber);
-				editor.selectAndReveal(lineInfo.getOffset(), lineInfo.getLength());
-				ISelection sel   = editor.getSelectionProvider().getSelection();
 
-				if (sel instanceof TextSelection) {
-					final TextSelection textSel = (TextSelection)sel;
-					return ((String) textSel.getText()).trim();
-				}
-			} catch(Exception e) {}
-		}		
-		return "";
-	}
-	
-	private ITextSelection getSelection()
-	{
-		return (org.eclipse.jface.text.ITextSelection) this.getPage().getSelection();
-	}
-	
-	private int getCurrentLineNumber(ITextSelection textSelection)
-	{
-		return textSelection.getStartLine();
-	}
-	
-	private ITextEditor getEditor()
-	{
-		try {
-			IEditorPart part = this.getPage().getActiveEditor();
-			if (part instanceof ITextEditor) {
-				return (ITextEditor)part;
-			}
-		} catch (Exception ex) {}
-		return null;
-	}
-	
-	private IDocument getDocument()
-	{
-		ITextEditor editor = this.getEditor();
-		if (editor != null) {
-			IDocument doc = editor.getDocumentProvider().getDocument(editor.getEditorInput());
-			if (doc != null) {
-				return doc;
-			}
+	private ITextEditor getEditor() {
+	    try {
+		IEditorPart part = this.getPage().getActiveEditor();
+		if (part instanceof ITextEditor) {
+		    return (ITextEditor) part;
 		}
-		return null;
-	}
-	
-	private IWorkbenchPage getPage()
-	{
-		return PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
-	}
-	
-	private Boolean useSpacesForTabs()
-	{
-		return Platform.getPreferencesService().getBoolean("org.eclipse.ui.editors", "spacesForTabs", false, null);
-	}
-	
-	private String getTabChar()
-	{
-		Boolean useSpaces = this.useSpacesForTabs();
-		int tabWidth      = Platform.getPreferencesService().getInt("org.eclipse.ui.editors", "tabWidth", 4, null);
-		return (!useSpaces) ? "\t" : String.format("%"+tabWidth+"s", "");
+	    } catch (Exception e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	    }
+	    return null;
 	}
 
-	private String toCamelCase(final String init) {
-		final StringBuilder ret = new StringBuilder(init.length());
-		for (final String word : init.split(" ")) {
-			ret.append(StringUtils.capitalize(word));
-		}
-		return ret.toString();
+	private ITextSelection getSelection() {
+	    return (org.eclipse.jface.text.ITextSelection) this.getPage()
+		    .getSelection();
 	}
+
+	public int getCurrentLineNumber() {
+	    return this.getSelection().getStartLine();
+	}
+
+	private IDocument getDocument() {
+	    ITextEditor editor = this.getEditor();
+	    if (editor != null) {
+		IDocument doc = editor.getDocumentProvider().getDocument(
+			editor.getEditorInput());
+		if (doc != null) {
+		    return doc;
+		}
+	    }
+	    return null;
+	}
+
+	public String getTabChar() {
+	    Boolean useSpaces = this.useSpacesForTabs();
+	    int tabWidth = Platform.getPreferencesService().getInt(
+		    "org.eclipse.ui.editors", "tabWidth", 4, null);
+	    return (!useSpaces) ? "\t" : String
+		    .format("%" + tabWidth + "s", "");
+	}
+
+	public Boolean useSpacesForTabs() {
+	    return Platform.getPreferencesService().getBoolean(
+		    "org.eclipse.ui.editors", "spacesForTabs", false, null);
+	}
+
+	public void generateTestMethod(String preparedText, int lineNumber,
+		IDocument doc) {
+	    ITextEditor editor = this.getEditor();
+	    if (!this.isValidEditor("php")) {
+		return;
+	    }
+	    try {
+		selectAndRevealText(lineNumber, doc, editor);
+		ISelection sel = editor.getSelectionProvider().getSelection();
+
+		if (sel instanceof TextSelection) {
+		    doc = this.insertMethod((TextSelection) sel, preparedText,
+			    doc);
+
+		    selectAndRevealText(lineNumber + 5, doc, editor);
+		}
+	    } catch (Exception e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	    }
+	}
+
+	private void selectAndRevealText(int lineNumber, IDocument doc,
+		ITextEditor editor) {
+
+	    IRegion lineInfo;
+	    try {
+		lineInfo = doc.getLineInformation(lineNumber);
+		editor.selectAndReveal(lineInfo.getOffset(),
+			lineInfo.getLength());
+	    } catch (BadLocationException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	    }
+	}
+
+	public IDocument insertMethod(TextSelection selection, String text,
+		IDocument document) {
+	    try {
+		document.replace(selection.getOffset(), selection.getLength(),
+			text);
+	    } catch (BadLocationException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	    }
+
+	    return document;
+	}
+
+	private Boolean isValidEditor(String languageSyntax) {
+	    Boolean isValid;
+	    ITextEditor editor = this.getEditor();
+	    if (editor == null
+		    || !editor.isEditable()
+		    || !this.getPage()
+			    .getActivePartReference()
+			    .getId()
+			    .toString()
+			    .toLowerCase()
+			    .contains(
+				    languageSyntax.trim().toString()
+					    .toLowerCase()
+					    + ".editor")) {
+		isValid = false;
+	    } else {
+		isValid = true;
+	    }
+	    return isValid;
+	}
+    }
 }
